ceeb6d076375f493188f20ce8ec6a8ca11f44bb0
Sachin21071997 <67464020+Sachin21071997@users.noreply.github.com>
2022-02-26 22:50:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function convert_to_human_date(epoch){
 }
 function componentToHex(c) {
   let hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+  return hex.length === 1 ? "0" + hex : hex;
 }
 function rgbToHex(red, green, blue) {
   red = parseInt(red);
@@ -114,11 +114,11 @@ function driver_function(userInput,wishTocompute){
     case 8:
       console.log(`You selected ${userInput}`);
       const bd_conversionInput = readLineSync.question(`Which conversion would you like to do:\na) Binary to Decimal\nb) Decimal to Binary\n`);
-      if(bd_conversionInput=="a"){
+      if(bd_conversionInput==="a"){
         const userInput = readLineSync.question(`Please enter the Binary Number to be converted to decimal:\n`);
         binTodec(userInput);
       }
-      else if(bd_conversionInput=="b"){
+      else if(bd_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Decimal Number to be converted to Binary:\n`);
         decTobin(userInput);
       }
@@ -129,11 +129,11 @@ function driver_function(userInput,wishTocompute){
     case 9:
       console.log(`You selected ${userInput}`);
       const bo_conversionInput = readLineSync.question(`Which conversion would you like to do:\na) Binary to Octal\nb) Octal to Binary\n`);
-      if(bo_conversionInput=="a"){
+      if(bo_conversionInput==="a"){
         const userInput = readLineSync.question(`Please enter the Binary Number to be converted to Octal:\n`);
         binTooct(userInput);
       }
-      else if(bo_conversionInput=="b"){
+      else if(bo_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Octal Number to be converted to Binary:\n`);
         octTobin(userInput);
       }
@@ -144,11 +144,11 @@ function driver_function(userInput,wishTocompute){
     case 10:
       console.log(`You selected ${userInput}`);
       const bh_conversionInput = readLineSync.question(`Which conversion would you like to do:\na) Binary to Hexadecimal\nb) Hexadecimal to Binary\n`);
-      if(bh_conversionInput=="a"){
+      if(bh_conversionInput==="a"){
         const userInput = readLineSync.question(`Please enter the Binary Number to be converted to Hexadecimal:\n`);
         binTohex(userInput);
       }
-      else if(bh_conversionInput=="b"){
+      else if(bh_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Hexadecimal Number to be converted to Binary:\n`);
         hexTobin(userInput);
       }
@@ -160,11 +160,11 @@ function driver_function(userInput,wishTocompute){
     case 11:
       console.log(`You selected ${userInput}`);
       const do_conversionInput = readLineSync.question(`Which conversion would you like to do:\na) Decimal to Octal\nb) Octal to Decimal\n`);
-      if(do_conversionInput=="a"){
+      if(do_conversionInput==="a"){
         const userInput = readLineSync.question(`Please enter the Decimal Number to be converted to Octal:\n`);
         decTooct(userInput);
       }
-      else if(do_conversionInput=="b"){
+      else if(do_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Octal Number to be converted to Decimal:\n`);
         octTodec(userInput);
       }
@@ -175,11 +175,11 @@ function driver_function(userInput,wishTocompute){
     case 12:
       console.log(`You selected ${userInput}`);
       const dh_conversionInput = readLineSync.question(`Which conversion would you like to do:\na) Decimal to Hexadecimal\nb) Hexadecimal to Decimal\n`);
-      if(dh_conversionInput=="a"){
+      if(dh_conversionInput==="a"){
         const userInput = readLineSync.question(`Please enter the Decimal Number to be converted to Hexadecimal:\n`);
         decTohex(userInput);
       }
-      else if(dh_conversionInput=="b"){
+      else if(dh_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Hexadecimal Number to be converted to Decimal:\n`);
         hexTodec(userInput);
       }
@@ -194,7 +194,7 @@ function driver_function(userInput,wishTocompute){
         const userInput = readLineSync.question(`Please enter the Octal Number to be converted to Hexadecimal:\n`);
         octTohex(userInput);
       }
-      else if(oh_conversionInput=="b"){
+      else if(oh_conversionInput==="b"){
         const userInput = readLineSync.question(`Please enter the Hexadecimal Number to be converted to Octal:\n`);
         hexTooct(userInput);
       }
@@ -205,13 +205,13 @@ function driver_function(userInput,wishTocompute){
     case 14:
       console.log(`You selected ${userInput}`);
       const RGB_hexinput = readLineSync.question(`Which conversion would you like to do:\na) RGB to Hexadecimal\nb) Hexadecimal to RGB\n`);
-      if(RGB_hexinput=="a"){
+      if(RGB_hexinput==="a"){
         const RedColorInput = readLineSync.question(`Please enter the value of the Red color:\n`);
         const GreenColorInput = readLineSync.question(`Please enter the value of the Green color:\n`);
         const BlueColorInput = readLineSync.question(`Please enter the value of the Blue color:\n`);
         rgbToHex(RedColorInput,GreenColorInput,BlueColorInput);
       }
-      else if(RGB_hexinput=="b"){
+      else if(RGB_hexinput==="b"){
         const HexInput = readLineSync.question(`Please enter the Hexadecimal value to be converted to RGB:\n`);
         HexTorgb(HexInput);
       }
@@ -222,19 +222,19 @@ function driver_function(userInput,wishTocompute){
     case 15:
       console.log(`You selected ${userInput}`);
       const unitInput = readLineSync.question(`Which conversion would you like to do:\na) cm to metres\nb) metres to cm\nc) Celsius to Fahrenheit\nd) Fahrenheit to Celsius\n`);
-      if(unitInput=="a"){
+      if(unitInput==="a"){
         const userInput = readLineSync.question(`Please enter the value in cm's to be converted to metres:\n`);
         cmTometres(userInput);
       }
-      else if(cunitInput=="b"){
+      else if(cunitInput==="b"){
         const userInput = readLineSync.question(`Please enter the value in metres to be converted to cm's:\n`);
         metresTocm(userInput);
       }
-      else if(unitInput=="c"){
+      else if(unitInput==="c"){
         const userInput = readLineSync.question(`Please enter the value in Celsius to be converted to Fahrenheit:\n`);
         console.log(FahToCels(userInput));
       }
-      else if(unitInput=="d"){
+      else if(unitInput==="d"){
         const userInput = readLineSync.question(`Please enter the value in Fahrenheit to be converted to Celsius:\n`);
         console.log(CelsToFah(userInput));
       }
@@ -249,10 +249,10 @@ function driver_function(userInput,wishTocompute){
 }
 function utilityConverter(wishTocompute){
   let userInput = readLineSync.question(`Welcome to the Utility App. Which utility function would you like to use\n1. Url Encoder\n2. Url Decoder\n3. Base64 Encoder\n4. Base64 Decoder\n5. String Hashing\n6. Epoch to Human Date Converter\n7. Human Date to Epoch Converter\n8. Binary/Decimal Converter\n9. Binary/Octal Converter\n10.Binary/Hexadecimal Converter\n11.Decimal/Octal Converter\n12.Decimal/Hexadecimal Converter\n13.Octal/Hexadecimal Converter\n14.RGB/Hex Converter\n15.Unit Converters\n`);
-  while(wishTocompute.toUpperCase()=="YES"){
+  while(wishTocompute.toUpperCase()==="YES"){
     driver_function(userInput,wishTocompute);
     wishTocompute = readLineSync.question(`Do you wish to compute again yes/no\n`);
-    if(wishTocompute.toUpperCase()=="YES"){
+    if(wishTocompute.toUpperCase()==="YES"){
       userInput = readLineSync.question(`Welcome to the Utility App. Which utility function would you like to use\n1. Url Encoder\n2. Url Decoder\n3. Base64 Encoder\n4. Base64 Decoder\n5. String Hashing\n6. Epoch to Human Date Converter\n7. Human Date to Epoch Converter\n8. Binary/Decimal Converter\n9. Binary/Octal Converter\n10.Binary/Hexadecimal Converter\n11.Decimal/Octal Converter\n12.Decimal/Hexadecimal Converter\n13.Octal/Hexadecimal Converter\n14.RGB/Hex Converter\n15.Unit Converters\n`); 
     }
     
